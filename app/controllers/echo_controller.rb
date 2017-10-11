87d1a0ea55a5706f790d597745cc889c88eab3f6
Justin Gondron <jgondron@nd.edu>
2017-10-11 11:01:53 -0400

    testing changes of different rails files

--- app/controllers/echo_controller.rb
+++ app/controllers/echo_controller.rb
@@ -1,5 +1,9 @@
 class EchoController < ApplicationController
   def index
-    render json: { message: params[:message] }
+    render json: {
+      message: params[:message],
+      secret: Rails.application.secrets.some_other_secret,
+      database: Rails.application.config.database_configuration['development']['some_other_config']
+    }
   end
 end
