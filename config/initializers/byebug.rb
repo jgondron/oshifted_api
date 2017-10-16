412154582b3dd87622337f771b7bfbcf56b93ab6
Justin Gondron <jgondron@nd.edu>
2017-10-16 10:38:46 -0400

    Removed debug output

--- config/initializers/byebug.rb
+++ config/initializers/byebug.rb
@@ -1,5 +1,4 @@
 if Rails.env.development?
-  print "I'm in byebug.rb"
   require 'byebug/core'
   #Byebug.wait_connection = true
   Byebug.start_server 'localhost', ENV.fetch("BYEBUG_SERVER_PORT", 9876).to_i rescue nil
