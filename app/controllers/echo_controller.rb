ae4a54fe662e3157a40eb4596e2fbefb0c72e45e
hparekh <hparekh@nd.edu>
2017-10-18 09:45:52 -0400

    Adding additional messages

--- app/controllers/echo_controller.rb
+++ app/controllers/echo_controller.rb
@@ -2,6 +2,8 @@ class EchoController < ApplicationController
   def index
     render json: {
       message: params[:message],
+      message1: params[:message],
+      message2: params[:message],
       secret: Rails.application.secrets.some_other_secret,
       database: Rails.application.config.database_configuration['development']['some_other_config']
     }
